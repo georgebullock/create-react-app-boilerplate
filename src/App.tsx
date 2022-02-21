630dc179f64944ca3f029f4d9560c7f22aded674
George <george.bullock@gmx.de>
2022-02-21 01:58:40 +0100

    Add styled-components theme support

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,39 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "./constants/routes";
+import Theming from "./styles/Theming";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Create React App Boilerplate</p>
-        <a
-          className="App-link"
-          href="https://gitlab.com/georgebullock/cra-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          georgebullock/cra-boilerplate
-        </a>
-        <div>
-          <Routes>
-            {ROUTES.map((route) => {
-              console.log("route.component:", route.component);
-              return (
-                <Route
-                  path={route.path}
-                  key={route.key}
-                  element={route.component}
-                />
-              );
-            })}
-          </Routes>
-        </div>
-      </header>
-    </div>
+    <Theming>
+      <div className="App">
+        <header className="App-header">
+          <p>Create React App Boilerplate</p>
+          <a
+            className="App-link"
+            href="https://gitlab.com/georgebullock/cra-boilerplate"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            georgebullock/cra-boilerplate
+          </a>
+          <div>
+            <Routes>
+              {ROUTES.map((route) => {
+                console.log("route.component:", route.component);
+                return (
+                  <Route
+                    path={route.path}
+                    key={route.key}
+                    element={route.component}
+                  />
+                );
+              })}
+            </Routes>
+          </div>
+        </header>
+      </div>
+    </Theming>
   );
 }
 
--- src/components/Container/Container.styles.ts
+++ src/components/Container/Container.styles.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  background-color: white;
+  background-color: ${({ theme }) => theme.colors.blue[100]};
+  border: 1rem solid ${({ theme }) => theme.colors.grey[0]};
+
+  & * {
+    color: ${({ theme }) => theme.colors.grey[0]};
+  }
 `;
